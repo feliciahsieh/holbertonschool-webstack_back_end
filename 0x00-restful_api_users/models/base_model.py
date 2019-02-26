a7c1d0e82b0e38c6549598d45705d200adde76fc
feliciahsieh <felicia@tixwriteoff.com>
2019-02-26 21:26:28 +0000

    first commit with code

---
+++ 0x00-restful_api_users/models/base_model.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""base_model.py"""
+
+import datetime
+import uuid
+
+class BaseModel:
+    """
+    BaseModel definition
+    """
+    id = None
+    created_at = None
+    updated_at = None
+
+    def __init__(self):
+        """ __init__() - initialized BaseModel object
+        Arguments: N/A
+        Returns: N/A
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.datetime.now()
