b0482a5ff80ee7c9ec91b0b5a355c7c4842a8f31
feliciahsieh <felicia@tixwriteoff.com>
2019-02-27 23:57:40 +0000

    replace self.id with self.email

--- 0x00-restful_api_users/models/user.py
+++ 0x00-restful_api_users/models/user.py
@@ -56,17 +56,17 @@ class User(BaseModel):
         """
         display_name() - displays full name of User instance
         """
-        if self.id is None and self.first_name is None \
+        if self.email is None and self.first_name is None \
            and self.last_name is None:
             return ""
 
         if self.first_name is None and self.last_name is None:
             return self.email
 
-        if self.last_name is None and type(self.first_name) is str:
+        if self.last_name is None:
             return self.first_name
 
-        if self.first_name is None and type(self.last_name) is str:
+        if self.first_name is None:
             return self.last_name
 
         return "{} {}".format(self.first_name, self.last_name)
