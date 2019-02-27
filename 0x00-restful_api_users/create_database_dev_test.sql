ee9792bdc21807c631cacf1574e580c979cd61ac
feliciahsieh <felicia@tixwriteoff.com>
2019-02-27 23:20:22 +0000

    first commit with code

---
+++ 0x00-restful_api_users/create_database_dev_test.sql
@@ -0,0 +1,5 @@
+-- create_database_dev_test.sql
+-- Create database hbtn_yelp_dev
+CREATE DATABASE IF NOT EXISTS hbtn_yelp_dev;
+-- Create database hbtn_yelp_test
+CREATE DATABASE IF NOT EXISTS hbtn_yelp_test;
