fe6a24a726c83d36f7994a0e29a15d49e860c7b3
feliciahsieh <felicia@tixwriteoff.com>
2019-02-27 02:02:38 +0000

    first commit with code

--- 0x00-restful_api_users/models/user.py
+++ 0x00-restful_api_users/models/user.py
@@ -21,3 +21,22 @@ class User(BaseModel):
         self.first_name = None
         self.last_name = None
         super().__init__()
+
+    def display_name(self):
+        """
+        display_name() - displays full name of User instance
+        """
+        if self.id is None and self.first_name is None \
+           and self.last_name is None:
+            return ""
+
+        if self.first_name is None and self.last_name is None:
+            return self.email
+
+        if self.last_name is None:
+            return self.first_name
+
+        if self.first_name is None:
+            return self.last_name
+
+        return self.first_name + self.last_name
