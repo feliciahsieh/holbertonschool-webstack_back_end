f3e1790752890ba4d39a85eb5e564fbbeb6125e7
feliciahsieh <felicia@tixwriteoff.com>
2019-03-01 17:05:48 +0000

    wrap __init__() in a try/except

--- 0x00-restful_api_users/models/base_model.py
+++ 0x00-restful_api_users/models/base_model.py
@@ -28,8 +28,11 @@ class BaseModel:
         Arguments: N/A
         Returns: N/A
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.utcnow()
+        try:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.utcnow()
+        except Exception as e:
+            print(e)
 
     @classmethod
     def all(cls):
