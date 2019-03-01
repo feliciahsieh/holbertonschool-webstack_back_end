3612d4fbde73efe50b9e7fca4c7d0969cd7e0e87
feliciahsieh <felicia@tixwriteoff.com>
2019-03-01 11:58:29 +0000

    change created_at and updaed_at to datetime

--- 0x00-restful_api_users/models/base_model.py
+++ 0x00-restful_api_users/models/base_model.py
@@ -29,7 +29,7 @@ class BaseModel:
         Returns: N/A
         """
         self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = str(datetime.utcnow())
+        self.created_at = self.updated_at = datetime.utcnow()
 
     @classmethod
     def all(cls):
