48b692abdab67027378381a0d3de2a0f850e64de
feliciahsieh <felicia@tixwriteoff.com>
2019-03-04 18:00:54 +0000

    work in progress

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""4-mru_cache.py - MRU cache """
+from base_caching import *
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """ MRUCache class definition
+    """
+
+    def __init__(self):
+        """ __init__() - LRUCache constructor
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ put() - add entry to MRU cache
+        Arguments:
+        key: key for dictionary
+        item: item to add to dictionary
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) < BaseCaching.MAX_ITEMS:
+                order_of_keys = self.cache_data.keys()
+                self.cache_data[key] = item
+                myTup = [(key, self.cache_data[key]) for key in order_of_keys]
+                self.cache_data = OrderedDict(myTup)
+            else:
+                if key not in self.cache_data:
+                    value = self.cache_data.popitem(last=True)
+                    print("DISCARD: {}".format(value[0]))
+                    self.cache_data[key] = item
+                    self.cache_data.move_to_end(key)
+                else:
+                    self.cache_data[key] = item
+
+    def get(self, key):
+        """ get() - get entry from LIFO cache
+        Arguments:
+        key: key for dictionary
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        value = self.cache_data.popitem(last=True)
+        return value[0]
