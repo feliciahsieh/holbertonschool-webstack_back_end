3cacb5f19340015cc47d88a1a7852f13d93eef6b
feliciahsieh <felicia@tixwriteoff.com>
2019-03-15 13:55:15 +0000

    first commit with code

--- 0x02-restful_api_users/0-warmup_flask.py
+++ 0x02-restful_api_users/0-warmup_flask.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" 0-warmup_flask.py - start a flask app """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """ index() - creates index.html
+    Arguments: N/A
+    Returns: N/A
+    """
+    return "Holberton School"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    app.url_map.strict_slashes = False
