126b69cc58f21f35c200c1295b0ea49f9789d2f8
feliciahsieh <felicia@tixwriteoff.com>
2019-03-18 22:47:13 +0000

    linted

--- 0x02-restful_api_users/api/v1/app.py
+++ 0x02-restful_api_users/api/v1/app.py
@@ -9,10 +9,12 @@ from flask import render_template
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     """ page_not_found() - handle 404 error """
-    return jsonify({ "error": "Not found" })
+    return jsonify({"error": "Not found"})
+
 
 if __name__ == "__main__":
     envHost = os.environ.get('HBNB_API_HOST')
