ef1069e6f953fb63acdde70421d3847f7539cba8
feliciahsieh <felicia@tixwriteoff.com>
2019-03-18 22:23:04 +0000

    app.py

---
+++ 0x02-restful_api_users/api/v1/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" app.py """
+from api.v1.views import app_views
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+if __name__ == "__main__":
+    envHost = os.environ.get('HBNB_API_HOST')
+    envPort = int(os.environ.get('HBNB_API_PORT'))
+    app.run(host=envHost, port=envPort)
