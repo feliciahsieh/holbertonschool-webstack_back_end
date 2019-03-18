533cc67e6f5f6c33573be48529611f0ea52a4315
feliciahsieh <felicia@tixwriteoff.com>
2019-03-18 22:48:11 +0000

    linted

--- 0x02-restful_api_users/api/v1/views/index.py
+++ 0x02-restful_api_users/api/v1/views/index.py
@@ -3,7 +3,8 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route("/status", strict_slashes=False)
 def msgStatus():
     """msgStatus() - return status msg """
-    return jsonify({"status":"OK"})
+    return jsonify({"status": "OK"})
