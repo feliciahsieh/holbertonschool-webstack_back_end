2c9a9678f00d110fe4585bcd5a01fa26a5d1667f
feliciahsieh <felicia@tixwriteoff.com>
2019-03-18 22:34:00 +0000

    index.py

--- 0x02-restful_api_users/api/v1/views/index.py
+++ 0x02-restful_api_users/api/v1/views/index.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" index.py - index fil e """
+""" index.py - index file """
 from api.v1.views import app_views
 from flask import jsonify
 
 @app_views.route("/status", strict_slashes=False)
 def msgStatus():
-    """msgStatus - return status msg """
+    """msgStatus() - return status msg """
     return jsonify({"status":"OK"})
