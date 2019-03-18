f13a21e23a7289a3c94383582731660844596f7e
feliciahsieh <felicia@tixwriteoff.com>
2019-03-18 21:22:33 +0000

    fix text

--- 0x02-restful_api_users/0-warmup_flask.py
+++ 0x02-restful_api_users/0-warmup_flask.py
@@ -3,11 +3,15 @@
 
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index():
+    """index()- send text back on home page """
     return "Holberton School"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
