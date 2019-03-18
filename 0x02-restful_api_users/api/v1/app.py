7694d7d04f686259cbb8559374bf43da03a599d5
feliciahsieh <felicia@tixwriteoff.com>
2019-03-18 22:44:09 +0000

    #6 handle 404 error

--- 0x02-restful_api_users/api/v1/app.py
+++ 0x02-restful_api_users/api/v1/app.py
@@ -3,11 +3,16 @@
 from flask import Flask
 from api.v1.views import app_views
 import os
+from flask import render_template
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    """ page_not_found() - handle 404 error """
+    return jsonify({ "error": "Not found" })
 
 if __name__ == "__main__":
     envHost = os.environ.get('HBNB_API_HOST')
