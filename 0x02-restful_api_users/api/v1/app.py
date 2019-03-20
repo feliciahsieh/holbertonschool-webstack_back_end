61c268229d2cdaed5948e1881836f9ed1697b04f
feliciahsieh <felicia@tixwriteoff.com>
2019-03-20 22:54:11 +0000

    add comments to __init__ file

--- 0x02-restful_api_users/api/v1/app.py
+++ 0x02-restful_api_users/api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-app.py
+app.py - app file
 """
 import os
 from api.v1.views import app_views
@@ -20,3 +20,5 @@ if __name__ == "__main__":
     envHost = os.environ.get('HBNB_API_HOST')
     envPort = int(os.environ.get('HBNB_API_PORT'))
     app.run(host=envHost, port=envPort)
+    import doctest
+    doctest.testmod(verbose=True)
