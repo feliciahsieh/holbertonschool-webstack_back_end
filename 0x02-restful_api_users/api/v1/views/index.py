1684dea52b9da9080bd865d3c7f92c922d256cb2
feliciahsieh <felicia@tixwriteoff.com>
2019-03-21 00:37:29 +0000

    first commit with code

--- 0x02-restful_api_users/api/v1/views/index.py
+++ 0x02-restful_api_users/api/v1/views/index.py
@@ -2,9 +2,18 @@
 """ index.py - index file """
 from api.v1.views import app_views
 from flask import jsonify
+from models.user import User
 
 
 @app_views.route("/status", strict_slashes=False)
 def msgStatus():
     """msgStatus() - return status msg """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def msgStats():
+    """msgStats() - return stats msg """
+    r = {}
+    r["users"] = User.count()
+    return jsonify(r)
