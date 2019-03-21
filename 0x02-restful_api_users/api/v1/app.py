060fa188b8d1e51f1ba76b4f9d9014b6f443ce10
feliciahsieh <felicia@tixwriteoff.com>
2019-03-21 00:56:12 +0000

    #7 first commit with code

--- 0x02-restful_api_users/api/v1/app.py
+++ 0x02-restful_api_users/api/v1/app.py
@@ -5,6 +5,7 @@ app.py - app file sdklfa sklflasdf lasdflkj sadflk asdfasjk fd
 import os
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import db_session
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,6 +15,12 @@ app.register_blueprint(app_views)
 def page_not_found(e):
     """ page_not_found() - handle 404 error """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ close_db(error) - close database when req finished """
+    db_session.close()
 
 
 if __name__ == "__main__":
