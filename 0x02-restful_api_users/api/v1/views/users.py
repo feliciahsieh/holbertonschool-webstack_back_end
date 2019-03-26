83cf45939f58a07c971eb45bf2f39a81392de835
feliciahsieh <felicia@tixwriteoff.com>
2019-03-26 17:25:07 +0000

    latest. work in progress

--- 0x02-restful_api_users/api/v1/views/users.py
+++ 0x02-restful_api_users/api/v1/views/users.py
@@ -8,10 +8,11 @@ from models import db_session
 
 @app_views.route("/users", methods=["GET", "POST"], strict_slashes=False)
 def msgUsersListAll():
-    """ msgUsersListAll() - return a List all Users or create a User """
+    """ msgUsersListAll() - return a List all Users or Create a User """
     r = []
 
-    # Get All Users
+    # List All Users
+    # curl "http://0.0.0.0:5050/api/v1/users"
     if request.method == "GET":
         for user in db_session.query(User).all():
             print(user.to_dict())
@@ -19,12 +20,15 @@ def msgUsersListAll():
         return jsonify(r)
 
     # Create a User
+    # curl -i -X POST -H 'Content-Type: application/json' -d '{
+    # "first_name": "Felicia", "last_name": "Hsieh", "email": "123@abc.com",
+    # "password": "123"}' http://0.0.0.0:5050/api/v1/users
     if request.method == "POST":
         u = User()
-        u["first_name"] = "Felicia"
-        u["last_name"] = "Hsieh"
-        u["email"] = "214@holbertonschool.com"
-        u["password"] = "123"
+        u.first_name = "felicia"
+        u.last_name = "hsieh"
+        u.email = "123@abc.com"
+        u.password = "mypass"
         db_session.add(u)
         db_session.commit()
         return jsonify(u.to_dict()), 201
@@ -34,6 +38,8 @@ def msgUsersListAll():
                  strict_slashes=False)
 def msgUsersSingleEntry(user_id):
     """ msgUsersSingleEntry() - modify a single user accordingly """
+    print("****** IN SECOND USER FUNCTION: {} *****".format(user_id))
+
     u = User.get(user_id)
     if u is None:
         abort(404)
@@ -43,9 +49,23 @@ def msgUsersSingleEntry(user_id):
         return jsonify(u.to_dict())
 
     # Delete one User
+    # curl -X DELETE http://0.0.0.0:5050/api/v1/users/[user_id]
     if request.method == "DELETE":
         db_session.delete(u)
         db_session.commit()
 
+    # Update one User
+    # curl -i -X PUT -H 'Content-Type: application/json' -d
+    # '{"first_name": "Santa", "last_name": "Claus"}'
+    # http://0.0.0.0:5050/api/v1/users/[user_id]
     if request.method == "PUT":
-        pass
+        print("***PUT REQUEST for {}***".format(user_id))
+
+    if not request.json:
+        return make_request(jsonify(error="Wrong format"), 400)
+    if request.json.get('first_name'):
+        u.first_name = request.json.get('first_name')
+    if request.json.get('last_name'):
+        u.last_name = request.json.get('last_name')
+    db_session.commit()
+    return jsonify(u.to_dict())
