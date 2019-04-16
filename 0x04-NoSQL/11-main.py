c46c6b774b6dab9c661906a06a9234ff2013426f
feliciahsieh <felicia@tixwriteoff.com>
2019-04-16 14:58:21 +0000

    fix main files for pycodestyle

--- 0x04-NoSQL/11-main.py
+++ 0x04-NoSQL/11-main.py
@@ -10,15 +10,17 @@ if __name__ == "__main__":
     school_collection = client.my_db.school
 
     j_schools = [
-        { 'name': "Holberton school", 'topics': ["Algo", "C", "Python", "React"]},
-        { 'name': "UCSF", 'topics': ["Algo", "MongoDB"]},
-        { 'name': "UCLA", 'topics': ["C", "Python"]},
-        { 'name': "UCSD", 'topics': ["Cassandra"]},
-        { 'name': "Stanford", 'topics': ["C", "React", "Javascript"]}
+        {'name': "Holberton school",
+         'topics': ["Algo", "C", "Python", "React"]},
+        {'name': "UCSF", 'topics': ["Algo", "MongoDB"]},
+        {'name': "UCLA", 'topics': ["C", "Python"]},
+        {'name': "UCSD", 'topics': ["Cassandra"]},
+        {'name': "Stanford", 'topics': ["C", "React", "Javascript"]}
     ]
     for j_school in j_schools:
         insert_school(school_collection, **j_school)
 
     schools = schools_by_topic(school_collection, "Python")
     for school in schools:
-        print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('topics', "")))
+        print("[{}] {} {}".format(school.get('_id'), school.get('name'),
+                                  school.get('topics', "")))
