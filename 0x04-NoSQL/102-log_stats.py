d234d69e2947a4903d367291309cc558eda605db
feliciahsieh <felicia@tixwriteoff.com>
2019-04-17 05:11:43 +0000

    created empty file

---
+++ 0x04-NoSQL/102-log_stats.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+""" 102-log_stats.py - display top 10 highest IP's in collection, nginx,
+ in database named logs """
---
+++ 0x04-NoSQL/12-log_stats.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+""" 12-log_stats.py - display Nginx logs stored in mongodb """
